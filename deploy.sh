813417145e62b9c3f369f08b7830654db97e539c
Abhishek Pednekar <abhi_ap@hotmail.com>
2022-01-26 23:03:39 +0530

    Added the deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+set -xe
+
+server=157.245.101.118
+
+if [ $TRAVIS_BRANCH == 'main' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
+  rsync -a --exclude={"deploy.sh","travis_rsa.enc","alembic","alembic.ini","isort.cfg"} * travis@$server:/home/listings/api
+  echo "Deployed successfully!"
+else
+  echo "Not deploying, since the branch isn't main."
+fi
